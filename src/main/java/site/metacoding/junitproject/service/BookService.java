8c7ecb747d7aaac2f2b8cc7f53fcdeb9f6b8d736
jyyu <jyyu@seegene.com>
2022-10-12 13:39:48 +0900

    서비스 레이어 - 책 삭제하기(생략), 책 수정하기 테스트 완료

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -76,11 +76,12 @@ public class BookService {
 
     // 5. 책 수정
     @Transactional(rollbackFor = RuntimeException.class)
-    public void 책수정하기(Long id, BookSaveReqDto dto) { //id, title, author
+    public BookRespDto 책수정하기(Long id, BookSaveReqDto dto) { //id, title, author
         Optional<Book> bookOP = bookRepository.findById(id);
         if(bookOP.isPresent()) { // 찾았다면
             Book bookPS = bookOP.get();
             bookPS.update(dto.getTitle(), dto.getAuthor());
+            return bookPS.toDto();
         } else {
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
         }
