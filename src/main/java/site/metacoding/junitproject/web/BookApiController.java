6a78d4002ad63ecadd4c88f3a5eff7f04f3f41e9
jyyu <jyyu@seegene.com>
2022-10-13 09:28:44 +0900

    컨트롤러 레이어 본코드 - 책 등록 하기 - 예외 핸들링 수정

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import site.metacoding.junitproject.service.BookService;
 import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
 import site.metacoding.junitproject.web.dto.response.BookListRespDto;
@@ -35,25 +34,24 @@ public class BookApiController { // composition = has 관계
     // { "key": value, "key": value }
     @PostMapping("/api/v1/book")
     public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveReqDto bookSaveReqDto, BindingResult bindingResult) {
-        BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
-
         // System.out.println("==============================");
         // System.out.println(bindingResult.hasErrors());
         // System.out.println("==============================");
 
         // AOP 처리하는 게 좋음!!
         if(bindingResult.hasErrors()) {
-            Map<String, String> erroMap = new HashMap<>();
+            Map<String, String> errorMap = new HashMap<>();
             for(FieldError fe : bindingResult.getFieldErrors()) {
-                erroMap.put(fe.getField(), fe.getDefaultMessage());
+                errorMap.put(fe.getField(), fe.getDefaultMessage());
             }
             System.out.println("==============================");
-            System.out.println(erroMap.toString());
+            System.out.println(errorMap.toString());
             System.out.println("==============================");
 
-            throw new RuntimeException(erroMap.toString());
+            throw new RuntimeException(errorMap.toString());
         }
 
+        BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
         return new ResponseEntity<>(CMRespDto.builder().code(1).msg("글 저장 성공").body(bookRespDto).build(), HttpStatus.CREATED); // 201 = insert
     }
 
